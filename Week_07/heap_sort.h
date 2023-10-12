8497bf53b5f0118803e869d9ef827db4a7f31f24
Andreas-Bugge <202204219@post.au.dk>
2023-10-12 12:31:56 +0200

    Exercise 3

--- Week_07/heap_sort.h
+++ Week_07/heap_sort.h
@@ -11,7 +11,10 @@ template <typename Comparable>
 void minHeapify(vector<Comparable> &array, int node, int n) {
 	int child;
 	Comparable tmp = std::move(array[node]);
-
+	// his condition ensures that the loop continues as long as the left child of the current node is within the bounds of the heap.
+	// The left child index is calculated as (node * 2 + 1), which represents the position of the left child in a binary heap.
+	// This part is O(log N) because, in the worst case, the loop will run for each level of the binary heap, 
+	// and the height of a binary heap is logarithmic with respect to the number of elements (N).
 	for (; node * 2 + 1 < n; node = child) {
 		child = node * 2 + 1;
 		if (child != n - 1 && array[child + 1] < array[child])
@@ -29,9 +32,13 @@ void minHeapify(vector<Comparable> &array, int node, int n) {
  */
 template <typename Comparable>
 void heapSort(vector<Comparable> &array) {
+	// The loop runs for approximately N/2 iterations, and within each iteration, 
+	// the minHeapify function is called. The time complexity of minHeapify is O(log N).
 	for (int i = array.size() / 2 - 1; i >= 0; --i)	/* buildHeap */
 		minHeapify(array, i, array.size());
-
+	// After building the heap, the code performs heap sort by repeatedly extracting the minimum element. 
+	// The outer loop runs for N-1 iterations, and within each iteration, minHeapify is called. 
+	// Since minHeapify has a time complexity of O(log N), the overall time complexity of this part is O(N * log N).
 	for (int j = array.size() - 1; j > 0; --j) {
 		std::swap(array[0], array[j]);	/* deleteMin */
 		minHeapify(array, 0, j);
